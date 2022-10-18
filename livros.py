9aa0254a6d7880c710ed93de795561b0ba86dbb0
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2022-10-18 14:02:52 -0300

    Primeira API

---
+++ livros.py
@@ -0,0 +1,21 @@
+# fazer o import no arquivo app.py para melhor organização
+
+livros = [
+    {
+        'id': 1,
+        'título': 'O senhor dos aneis - A sociedade do anel',
+        'autor': 'J.R.R Tolkien'
+    },
+
+    {
+        'id': 2,
+        'título': 'Harry Potter e a pedra filosofal',
+        'autor': 'J.K Howling'
+    },
+
+    {
+        'id': 3,
+        'título': 'James Clear',
+        'autor': 'Hábitos Atômicos'
+    },
+]
